3e92b9ca15efc98f69ca714624a622e52667317b
sheikice <sheikouz@gmail.com>
2026-06-18 01:48:31 +0200

    Replace 'docker.io' with 'docker' in installation

--- bot.sh
+++ bot.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 sed -i '/^deb cdrom:/s/^/#/' /etc/apt/sources.list
-apt-get update && apt-get install -y sudo openssh-server xfce4 lightdm firefox-esr docker.io docker-compose git make ca-certificates && usermod -aG sudo,docker $USERNAME && systemctl enable ssh docker lightdm
+apt-get update && apt-get install -y sudo openssh-server xfce4 lightdm firefox-esr docker docker-compose git make ca-certificates && usermod -aG sudo,docker $USERNAME && systemctl enable ssh docker lightdm
