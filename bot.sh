d917d4ac76513ec24f99c7f1ad5e9a895f80cfe2
sheikice <sheikouz@gmail.com>
2026-06-18 01:36:28 +0200

    Fix package manager command

--- bot.sh
+++ bot.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-apt update && apt install -y sudo openssh-server xfce4 lightdm firefox-esr docker.io docker-compose-plugin git make ca-certificates && usermod -aG sudo,docker $USERNAME && systemctl enable ssh docker lightdm
+sed -i '/^deb cdrom:/s/^/#/' /etc/apt/sources.list
+apt-get update && apt-get install -y sudo openssh-server xfce4 lightdm firefox-esr docker.io docker-compose-plugin git make ca-certificates && usermod -aG sudo,docker $USERNAME && systemctl enable ssh docker lightdm
