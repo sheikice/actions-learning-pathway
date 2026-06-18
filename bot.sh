3e29ff5e6e76673d55629ebcdf48d18298ccfa5e
sheikice <sheikouz@gmail.com>
2026-06-18 08:31:56 +0200

    Fix variable

--- bot.sh
+++ bot.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
+USERNAME=skc
 
 sed -i '/^deb cdrom:/s/^/#/' /etc/apt/sources.list
-apt-get update && apt-get install -y sudo openssh-server xfce4 lightdm firefox-esr docker docker-compose git make ca-certificates && usermod -aG sudo,docker $USERNAME && systemctl enable ssh docker lightdm
+apt-get update && apt-get install -y sudo openssh-server xfce4 lightdm firefox-esr docker docker-compose git make ca-certificates && usermod -aG sudo,docker "${USERNAME}" && systemctl enable ssh docker lightdm
